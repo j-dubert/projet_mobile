f0c33537ffdb5bd13e36843dc03a478265e1f65d
Julien <j.dubert12@gmail.com>
2022-01-05 01:08:15 +0100

    ciao

---
+++ app/src/main/java/com/example/projetmobile/testprovider.java
@@ -0,0 +1,24 @@
+package com.example.projetmobile;
+
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.RemoteException;
+import android.widget.Toast;
+
+public class testprovider {
+
+    private static Uri provideruri = Uri.parse("content://com.example.appcontact.provider/T_Contacts");
+
+    public static void testprovidertest (Context context) throws RemoteException {
+        ContentProviderClient yourCR =  context.getContentResolver().acquireContentProviderClient(provideruri );
+        Cursor c =yourCR.query(provideruri, null, "*", null, null );
+        if (c != null){
+            System.out.println("--------------------------fuck yeah------------------------------");
+        }else{
+            System.out.println("fuck no");
+        };
+    }
+}
